da8d2e2a7f43df862033fba5e8df808be0f9a4e2
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-04-05 08:31:30 +0500

    Update app.py

--- 07_bmi_calculator/app.py
+++ 07_bmi_calculator/app.py
@@ -8,3 +8,11 @@ weight = st.slider("Enter your weight (in kg) :",40,150,70)
 
 bmi = weight / ((height/100)**2)
 st.success(f"Your Boddy Mass Index is {bmi:.2f}.")
+
+st.write("### BMI Categories: ###")
+st.write(" - UnderWeight: BMI less than 18.5")  
+st.write(" - NormalWeight: BMI between 18.5 and 24.9")
+st.write(" - OverWeight: BMI between 25 and 29.9")
+st.write(" - Obesity: BMI 30 or above")
+
+st.write("Made with 🤍 by Muhammad Hammad ur Rehman")
